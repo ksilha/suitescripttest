0ecb387d6e0a23e7f1073923f2357b974c9bec2d
Kevin <ksilha@gmail.com>
2016-06-01 10:28:00 -0500

    First SuiteScript Project Test

---
+++ Kevin's Test/gl_plugin_test.js
@@ -0,0 +1,25 @@
+/**
+ * Module Description
+ * 
+ * Version Date Author Remarks 1.00 02 Apr 2015 Kevin
+ * 
+ */
+
+function customizeGlImpact(transactionRecord, standardLines, customLines, book) {
+	for (var i = 0; i < standardLines.getCount(); i++) {
+		var currLine = standardLines.getLine(i);
+		var newLine = customLines.addNewLine();
+		
+		if (currLine.getCreditAmount() != 0){
+			newLine.setCreditAmount(currLine.getCreditAmount());
+			newLine.setAccountId(currLine.getAccountId());
+		}
+		else{
+			newLine.setDebitAmount(currLine.getDebitAmount());
+			newLine.setAccountId(54);
+		}
+		
+		
+		newLine.setMemo("Payment catches both revenue and cash.");
+	}
+}
